3b80a6c444d25ae0e98057ad7ba823218eafd6eb
(Phong)Trần Hải Phong <tranhaiphong2016fpt@gmail.com>
2023-11-18 17:31:59 +1100

    Update settings.php

--- lab10/settings.php
+++ lab10/settings.php
@@ -5,6 +5,6 @@
 // $sql_db = "s104334842_db"; // your database
 $host = "feenix-mariadb.swin.edu.au";
 $user = "s104334842"; // your user name
-$pwd = "@taoXincu17"; // your password (date of birth ddmmyy unless changed)
+$pwd = ""; // your password (date of birth ddmmyy unless changed)
 $sql_db = "s104334842_db"; // your database
 ?>
