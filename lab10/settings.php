acc9c6c47fa28223231385865e24167c1db2bb2d
(Phong)Trần Hải Phong <tranhaiphong2016fpt@gmail.com>
2023-11-18 17:32:42 +1100

    Update settings.php

--- lab10/settings.php
+++ lab10/settings.php
@@ -2,9 +2,9 @@
 // $host = "localhost";
 // $user = "root"; // your user name
 // $pwd = ""; // your password (date of birth ddmmyy unless changed)
-// $sql_db = "s104334842_db"; // your database
+// $sql_db = ""; // your database
 $host = "feenix-mariadb.swin.edu.au";
-$user = "s104334842"; // your user name
+$user = ""; // your user name
 $pwd = ""; // your password (date of birth ddmmyy unless changed)
-$sql_db = "s104334842_db"; // your database
+$sql_db = ""; // your database
 ?>
